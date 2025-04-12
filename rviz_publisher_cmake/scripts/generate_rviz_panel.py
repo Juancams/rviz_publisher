cc720525dcbdcaf2a1965269340d39451c4b792e
Juancams <jc.manzanares.serrano@gmail.com>
2025-04-12 13:08:43 +0200

    Updated script and templates to create buttons that publish msgs
    Signed-off-by: Juancams <jc.manzanares.serrano@gmail.com>

--- rviz_publisher_cmake/scripts/generate_rviz_panel.py
+++ rviz_publisher_cmake/scripts/generate_rviz_panel.py
@@ -20,8 +20,27 @@ def get_message_header_from_type(msg_type):
             output.append(msg_type_list[i])
     return ''.join(output) + '.hpp'
 
+def generate_message_setters(msg_dict, prefix="msg"):
+    code_lines = []
+
+    def recurse(d, path):
+        for k, v in d.items():
+            full_path = f"{path}.{k}"
+            if isinstance(v, dict):
+                recurse(v, full_path)
+            else:
+                if isinstance(v, str):
+                    value = f'"{v}"'
+                elif isinstance(v, bool):
+                    value = "true" if v else "false"
+                else:
+                    value = str(v)
+                code_lines.append(f"{full_path} = {value};")
+
+    recurse(msg_dict, prefix)
+    return "\n    ".join(code_lines)
+
 def generate_code(yaml_file, header_out, cpp_out, dependencies_out, xml_out, templates_dir, project_name):
-    # Leer plantillas desde archivos
     header_template = read_template(os.path.join(templates_dir, "panel.hpp.j2"))
     cpp_template = read_template(os.path.join(templates_dir, "panel.cpp.j2"))
     deps_template = read_template(os.path.join(templates_dir, "dependencies.cmake.j2"))
@@ -30,45 +49,67 @@ def generate_code(yaml_file, header_out, cpp_out, dependencies_out, xml_out, tem
     with open(yaml_file, "r") as f:
         data = yaml.safe_load(f)
 
-    publishers = data.get("panel", {}).get("publishers", [])
+    panels = data.get("panel", [])
 
     message_types = set()
-    pub_declarations = ""
-    pub_initializations = ""
     includes = ""
     find_dependencies = ""
     dependencies = ""
+    pub_declarations = ""
+    button_declarations = ""
+    pub_initializations = ""
+    button_initializations = ""
 
-    for pub in publishers:
+    for pub in panels:
+        name_raw = pub["name"]
+        name = name_raw.lower().replace(" ", "_")
         topic = pub["topic"]
-        msg_type = pub["type"].replace("/", "::")
-        var_name = topic.replace("/", "_").strip("_")
-        message_types.add(pub["type"])
+        msg_type = pub["topic_type"].replace("/", "::")
+        header_type = pub["topic_type"]
+        message_types.add(header_type)
 
-        pub_declarations += f'  rclcpp::Publisher<{msg_type}>::SharedPtr {var_name}_pub_;\n'
-        pub_initializations += f'  {var_name}_pub_ = node_->create_publisher<{msg_type}>("{topic}", 10);\n'
-        pub_initializations += f'  publishers_["{topic}"] = {var_name}_pub_;\n'
+        pub_declarations += f'  rclcpp::Publisher<{msg_type}>::SharedPtr {name}_pub_;\n'
+        button_declarations += f'  QPushButton * {name}_button;\n'
+
+        pub_initializations += f'  {name}_pub_ = node_->create_publisher<{msg_type}>("{topic}", 10);\n'
+        pub_initializations += f'  publishers_["{topic}"] = {name}_pub_;\n'
+
+        button_initializations += f'  {name}_button = new QPushButton("{name_raw}");\n'
+        button_initializations += f'  layout_->addWidget({name}_button);\n'
+        button_initializations += f'  connect({name}_button, &QPushButton::clicked, this, [this]() {{\n'
+
+        if "message" in pub:
+            msg_code = f'    {msg_type} msg;\n'
+            msg_code += f'    {generate_message_setters(pub["message"])}\n'
+            msg_code += f'    {name}_pub_->publish(msg);\n'
+        else:
+            msg_code = f'    {name}_pub_->publish({msg_type}());\n'
+
+        button_initializations += msg_code
+        button_initializations += f'  }});\n'
+
         find_dependencies += f'find_package({msg_type.split("::")[0]} REQUIRED)\n'
         dependencies += f'{msg_type.split("::")[0]}\n'
 
     for msg in message_types:
         includes += f'#include <{get_message_header_from_type(msg)}>\n'
 
-    # Crear directorios de salida si no existen
-    for path in [header_out, cpp_out, dependencies_out]:
-        os.makedirs(os.path.dirname(path), exist_ok=True)
+    os.makedirs(os.path.dirname(header_out), exist_ok=True)
+    os.makedirs(os.path.dirname(cpp_out), exist_ok=True)
+    os.makedirs(os.path.dirname(dependencies_out), exist_ok=True)
 
-    # Escribir archivos de salida con las plantillas llenas
     with open(header_out, "w") as hpp:
         hpp.write(header_template.format(
             message_includes=includes,
             publisher_declarations=pub_declarations,
+            button_declarations=button_declarations,
             project_name=project_name
         ))
 
     with open(cpp_out, "w") as cpp:
         cpp.write(cpp_template.format(
             publisher_initializations=pub_initializations,
+            buttons_initializations=button_initializations,
             project_name=project_name,
         ))
 
